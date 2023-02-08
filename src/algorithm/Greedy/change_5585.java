e8c09dcaada0da3e9be6825e5ea4fbb7b46f69f9
baechaeyoon <baechaeyoon@naver.com>
2023-02-08 19:44:51 +0900

    거스름돈

---
+++ src/algorithm/Greedy/change_5585.java
@@ -0,0 +1,19 @@
+package algorithm.Greedy;
+
+import java.util.Scanner;
+
+public class change_5585 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = 1000 - sc.nextInt();
+        int count = 0;
+        int[] arr = {500,100,50,10,5,1};
+        for(int i =0; i<arr.length; i++) {
+            if(N%arr[i]>=0) {
+                count += N/arr[i];
+                N = N%arr[i];
+            }
+        }
+        System.out.println(count);
+    }
+}
